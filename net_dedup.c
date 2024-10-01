5981acf44cf9a82b7473202cf8205408729c62c2
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-10-01 19:59:19 -0400

    Mmapping shared fingerprint cache upon nccl init

--- net_dedup.c
+++ net_dedup.c
@@ -1,14 +1,38 @@
 #include "net_dedup.h"
 
 
-
-
-
 // initializes network
 ncclResult_t netDedup_init(ncclDebugLogger_t logFunction) {
 	
+	pid_t pid = getpid();
+	printf("[Process %d] Initially loaded net dedup nccl plugin!\n", pid);
+
 	int num_net_devices = init_net_devices(net_dedup_state.net_devices);
 	net_dedup_state.num_net_devices = num_net_devices;
+
+	// mmap shard cache into memory...
+	int fd = shm_open(FINGERPRINT_CACHE_PATH, O_RDWR | O_CREAT | O_EXCL, 0660);
+	// already exists
+	if (fd == -1){
+		while (fd == -1){
+			// wait just in case the other instance didnt finish the initialization
+			sleep(1);
+			fd = shm_open(FINGERPRINT_CACHE_PATH, O_RDWR, 0);
+		}
+		printf("[Process %d] Found existing fingerprint cache in system, and mmapping it in to address space!\n", pid);
+		net_dedup_state.global_fingerprint_cache = mmap(0,sizeof(Fingerprint_Cache),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+	}
+	// we just created it
+	else{
+		printf("[Process %d] Creating and initializing global fingerprint table & cache!\n\tTotal size (table + cache): %lu\n", pid, sizeof(Fingerprint_Cache));
+		ftruncate(fd, sizeof(Fingerprint_Cache));
+		net_dedup_state.global_fingerprint_cache = mmap(0,sizeof(Fingerprint_Cache),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+
+		// intialize the cache correctly
+		init_fingerprint_cache(net_dedup_state.global_fingerprint_cache);
+	}
+
+
 	return ncclSuccess;
 }
 
