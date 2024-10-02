50c6b9c4cd8582c140401c49f87a215a752ed44a
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-10-01 23:13:13 -0400

    Building again

--- plugin_netdedup.c
+++ plugin_netdedup.c
@@ -6,12 +6,15 @@
 #include "net_dedup.h"
 #include "net_dedup_coll.h"
 
+#define PLUGIN_NAME "Dedup"
+#define COLL_PLUGIN_NAME "CollDedup"
+
 
 Net_Dedup_State net_dedup_state;
 
 
 const ncclNet_v8_t ncclNetPlugin_v8 = {
-  .name = "DEDUP",
+  .name = PLUGIN_NAME,
   .init = netDedup_init,
   .devices = netDedup_devices,
   .getProperties = netDedup_getProperties_v8,
@@ -32,30 +35,8 @@ const ncclNet_v8_t ncclNetPlugin_v8 = {
   .irecvConsumed = netDedup_irecvConsumed,
 };
 
-const ncclCollNet_v8_t ncclCollNetPlugin_v8 = {
-  .name = "DEDUP_COLL",
-  .init = netDedup_init,
-  .devices = netDedup_devices,
-  .getProperties = netDedupColl_getProperties_v8,
-  .listen = netDedup_listen,
-  .connect = netDedupColl_connect,
-  .reduceSupport = netDedupColl_reduceSupport,
-  .regMr = netDedupColl_regMr_v8,
-  .regMrDmaBuf = netDedupColl_regMrDmaBuf,
-  .deregMr = netDedupColl_deregMr,
-  .iallreduce = netDedupColl_iallreduce,
-  .iallgather = netDedupColl_iallgather,
-  .ireducescatter = netDedupColl_ireducescatter,
-  .iflush = netDedupColl_iflush,
-  .test = netDedupColl_test,
-  .closeColl = netDedupColl_closeColl,
-  .closeListen = netDedupColl_closeListen,
-};
-
-
-
 const ncclNet_v7_t ncclNetPlugin_v7 = {
-  .name = "DEDUP",
+  .name = PLUGIN_NAME,
   .init = netDedup_init,
   .devices = netDedup_devices,
   .getProperties = netDedup_getProperties_v7,
@@ -76,8 +57,30 @@ const ncclNet_v7_t ncclNetPlugin_v7 = {
   .irecvConsumed = netDedup_irecvConsumed,
 };
 
+const ncclCollNet_v8_t ncclCollNetPlugin_v8 = {
+  .name = COLL_PLUGIN_NAME,
+  .init = netDedup_init,
+  .devices = netDedup_devices,
+  .getProperties = netDedupColl_getProperties_v8,
+  .listen = netDedup_listen,
+  .connect = netDedupColl_connect,
+  .reduceSupport = netDedupColl_reduceSupport,
+  .regMr = netDedupColl_regMr_v8,
+  .regMrDmaBuf = netDedupColl_regMrDmaBuf,
+  .deregMr = netDedupColl_deregMr,
+  .iallreduce = netDedupColl_iallreduce,
+  .iallgather = netDedupColl_iallgather,
+  .ireducescatter = netDedupColl_ireducescatter,
+  .iflush = netDedupColl_iflush,
+  .test = netDedupColl_test,
+  .closeColl = netDedupColl_closeColl,
+  .closeListen = netDedupColl_closeListen,
+};
+
+
+
 const ncclCollNet_v7_t ncclCollNetPlugin_v7 = {
-  .name = "DEDUP_COLL",
+  .name = COLL_PLUGIN_NAME,
   .init = netDedup_init,
   .devices = netDedup_devices,
   .getProperties = netDedupColl_getProperties_v7,
