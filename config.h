dad876faad0a143931531e52401db7dbf8233a37
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-10-05 20:47:47 -0400

    Maintaining logs

--- config.h
+++ config.h
@@ -23,9 +23,9 @@
 
 // High-level important info
 #define TO_LOG_CAPTURE_STATS 1 
-#define TO_LOG_GENERAL_HEADERS 0
-#define TO_LOG_FINGERPRINT_COMPUTATION 0
-#define TO_LOG_FINGERPRINT_HEADERS 0
+#define TO_LOG_GENERAL_HEADERS 1
+#define TO_LOG_FINGERPRINT_COMPUTATION 1
+#define TO_LOG_FINGERPRINT_HEADERS 1
 
 	
 // logs all entries and exit to internal functions 
